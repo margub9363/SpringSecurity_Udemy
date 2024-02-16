1db86ae85224d3382970224eface91f69b20d55c
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-16 22:56:54 +0530

    Added Spring Security Dependency

---
+++ springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welocme to spring Application with Security";
+    }
+}

f51b768f6c56e0cfe72f2d1dd5ff0a07d1bc121d
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-17 12:26:20 +0530

    Permitting all the incoming Requests

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->requests.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();

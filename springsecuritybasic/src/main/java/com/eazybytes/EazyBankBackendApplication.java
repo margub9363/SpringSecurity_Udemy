d507791ea613d7b15edd13c119764185a40f2ef3
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-17 11:41:40 +0530

    Setup for Session2

--- springsecuritybasic/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecuritybasic/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,13 +1,13 @@
-package com.eazybytes.springsecuritybasic;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringsecuritybasicApplication {
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
--- springsecuritybasic/src/main/java/com/eazybytes/controller/WelcomeController.java
+++ springsecuritybasic/src/main/java/com/eazybytes/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecuritybasic.controller;
+package com.eazybytes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

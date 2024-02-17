5b76d6513ab8d0e28a711cb80c086913c7787102
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-17 13:02:05 +0530

    Setup For Session3

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","myBalance","/myLoans","myCards").authenticated().requestMatchers("/notices","/contact").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();

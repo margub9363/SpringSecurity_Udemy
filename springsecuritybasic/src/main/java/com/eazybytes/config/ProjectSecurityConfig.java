282a6ba1574ba760ccd3fb3a55b996df07254731
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-17 12:21:46 +0530

    Denying all the incoming Requests

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","myBalance","/myLoans","myCards").authenticated().requestMatchers("/notices","/contact").permitAll());
+        http.authorizeHttpRequests((requests) ->requests.anyRequest().denyAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();

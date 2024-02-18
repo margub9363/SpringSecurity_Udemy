d07cf6ee75472c8df2da8218db76df0d28286494
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-02-18 13:54:03 +0530

    Endpoint to Register New User

--- springsecuritybasic/src/main/java/com/eazybytes/model/Customer.java
+++ springsecuritybasic/src/main/java/com/eazybytes/model/Customer.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO,generator ="native")
+    @GenericGenerator(name = "native",strategy = "native")
     private int id;
     private String email;
     private String pwd;
